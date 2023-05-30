fd1da958ad8347f3ca8e02fc302ab03ada1d7533
mayaoumana <oumana.raissi@gmail.com>
2023-05-30 09:23:33 +0200

    plus report

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,14 @@ const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", { downloadFile });
+      require("cypress-mochawesome-reporter/plugin")(on);
+
     },
   },
 });
